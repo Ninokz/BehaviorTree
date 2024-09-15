1311364624d21d06220ec23a9cc0aa8a4c7d7802
Ninokz <2606904530@qq.com>
2024-09-15 10:44:16 +0800

    添加项目文件。

---
+++ BehaviorTree/bht.cpp
@@ -0,0 +1,16 @@
+#include "BehaviorTree.h"
+
+
+int main()
+{
+	Nano::BehaviorTree::BehaviorTreeBuilder::Ptr builder = std::make_shared<Nano::BehaviorTree::BehaviorTreeBuilder>();
+	builder->Repeat("1", 3)
+				->Sequence("2")
+					->DebugNode("hello")
+					->DebugNode("world")
+					->DebugNode("fucker")
+				->Back()
+			->End();
+	builder->treeTick();
+	return 0;
+}
